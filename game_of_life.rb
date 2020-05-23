58a80ca7f4f60198cb6e14674acf422c0c247e29
Spencer Tassone <spencerwilliamtassone@gmail.com>
2020-05-23 11:59:13 -0400

    updated spec and basic methods

--- game_of_life.rb
+++ game_of_life.rb
@@ -1,1 +1,9 @@
 #root_file
+
+class World
+    attr_accessor :rows, :cols
+    def initialize(rows=3, cols=3)
+        @rows = rows
+        @cols = cols
+    end
+end
--- spec_game_of_life.rb
+++ spec_game_of_life.rb
@@ -1,1 +1,19 @@
 #describe game of life
+require 'rspec'
+require_relative "game_of_life.rb"
+describe 'game of life' do 
+    context 'world' do
+        subject { World.new}
+
+        it 'should create a new world object' do
+            subject.is_a?(World).should be true
+        end
+
+        it 'should respond to proper methods' do
+            subject.should respond_to(:rows)
+            subject.should respond_to(:cols)
+        end
+
+        
+    end
+end
