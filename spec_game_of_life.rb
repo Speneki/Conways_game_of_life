6e6aade25e85a1dd95311b60557ab91948d945c4
Spencer Tassone <spencerwilliamtassone@gmail.com>
2020-05-23 12:05:30 -0400

    create grid

--- spec_game_of_life.rb
+++ spec_game_of_life.rb
@@ -1,6 +1,6 @@
-#describe game of life
 require 'rspec'
 require_relative "game_of_life.rb"
+
 describe 'game of life' do 
     context 'world' do
         subject { World.new}
@@ -12,8 +12,15 @@ describe 'game of life' do
         it 'should respond to proper methods' do
             subject.should respond_to(:rows)
             subject.should respond_to(:cols)
+            subject.should respond_to(:cell_grid)            
         end
 
-        
+        it 'should create array grid on init' do
+            subject.cell_grid.is_a?(Array).should be true
+            subject.cell_grid.each do |row|
+                row.is_a?(Array).should be true
+            end
+        end
+
     end
 end
